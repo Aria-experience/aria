3b0297c68bacfb57bbff604d9d53c661db3100ef
Melissa Ugas <melugas@gmail.com>
2018-10-20 19:28:44 -0700

    Add GIBS data object

---
+++ src/data/gibs.js
@@ -0,0 +1,19 @@
+export default {
+    modisReflectance: {
+        title: 'MODIS Terra Visual',
+        productName: 'MODIS_Terra_CorrectedReflectance_TrueColor'
+    },
+    humanPop: {
+        title: 'Humans',
+        productName: 'Human_Footprint_1995-2004'
+    },
+    vegetation: {
+        title: 'Vegetation',
+        productName: 'MODIS_Terra_NDVI_8Day'
+    },
+    biosphere: {title: 'Biosphere', productName: 'Last_of_the_Wild_1995-2004'},
+    fire: {
+        title: 'Fire',
+        productName: 'MODIS_Fires_Terra'
+    }
+};
