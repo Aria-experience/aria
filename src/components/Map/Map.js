ce09846772f85ac83656c472caecf3f42f9aa696
Orestis Herodotou <orestis@digitaltopo.net>
2018-10-20 17:53:32 -0700

    Add layer spy

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -24,6 +24,12 @@ import {MapContainer} from './Map.styles';
 class AppMap extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            radius: 10
+        };
+
+        this.mousePosition = null;
     }
 
     componentDidMount() {
@@ -50,6 +56,41 @@ class AppMap extends Component {
                 maxZoom: 20
             })
         });
+
+        // Add mouse listener
+        this.map.on('pointermove', event => {
+            console.log('event', event);
+            this.mousePosition = event.pixel;
+            console.log('this.mousePosition', this.mousePosition);
+            this.map.render();
+        });
+    };
+
+    // Layer spy compose
+    spyCompose = (type, event) => {
+        const ctx = event.context;
+
+        if (type === 'postcompose') {
+            ctx.restore();
+        } else {
+            const pixelRatio = event.frameState.pixelRatio;
+            ctx.save();
+            ctx.beginPath();
+            if (this.mousePosition) {
+                // only show a circle around the mouse
+                ctx.arc(
+                    this.mousePosition[0] * pixelRatio,
+                    this.mousePosition[1] * pixelRatio,
+                    this.state.radius * pixelRatio,
+                    0,
+                    2 * Math.PI
+                );
+                ctx.lineWidth = 5 * pixelRatio;
+                ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+                ctx.stroke();
+            }
+            ctx.clip();
+        }
     };
 
     // Creates the data layer
@@ -62,15 +103,25 @@ class AppMap extends Component {
             'MODIS_Terra_CorrectedReflectance_TrueColor/default/2013-06-15/' +
             'GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg';
 
+        // Create Layer Source
         this.source = new XYZ({
             url: testUrl
             //url: createDataLayerXYZUrl(product, date)
         });
 
+        // Create Layer
         this.dataLayer = new TileLayer({
             source: this.source,
             zIndex: 1
         });
+
+        // Attach "compose" listeners
+        this.dataLayer.on('precompose', event =>
+            this.spyCompose('precompose', event)
+        );
+        this.dataLayer.on('postcompose', event =>
+            this.spyCompose('postcompose', event)
+        );
 
         return this.dataLayer;
     };
