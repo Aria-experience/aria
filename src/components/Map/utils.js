9eedc0e75574af682c6856e3703e79e414e3163d
Orestis Herodotou <orestis@digitaltopo.net>
2018-10-27 18:53:07 -0700

    Add GIBS list fetch

--- src/components/Map/utils.js
+++ src/components/Map/utils.js
@@ -1,8 +1,13 @@
+import WMTSCapabilities from 'ol/format/WMTSCapabilities.js';
+import {optionsFromCapabilities} from 'ol/source/WMTS.js';
+
+// App
 import {
     GIBS_BASE_URL,
     GIBS_DEFAULT_SUFFIX,
     XYZ_SUFFIX,
-    DEFAULT_FORMAT_SUFFIX
+    DEFAULT_FORMAT_SUFFIX,
+    GIBS_CAPABILITIES_URL
 } from './constants';
 
 // Creates the data layer url
@@ -18,3 +23,12 @@ export const createDataLayerXYZUrl = (
     `${baseUrl || GIBS_BASE_URL}/${productName}/default/${
         date ? date + '/' : ''
     }${urlSuffix || GIBS_DEFAULT_SUFFIX}/${XYZ_SUFFIX}${fileFormat}`;
+
+// Define the WMTS Capabilities Parser
+const parser = new WMTSCapabilities();
+
+// Get Capabilities from GIBS
+export const fetchGibsCapabilities = () =>
+    fetch(GIBS_CAPABILITIES_URL)
+        .then(response => response.text())
+        .then(text => parser.read(text));
